85353f6e0355d030eb1a1fd1242a0be51304f3d5
Nicholas Malaya <nicholas.malaya@gmail.com>
2014-02-25 18:04:16 -0600

    updating with a matlab routine, that needs to be reimplemented

--- inv_prob/ps2/in-cg.py
+++ inv_prob/ps2/in-cg.py
@@ -7,6 +7,32 @@
 #
 import numpy as np
 
+def mycg(A,b,maxiter,tol,x):
+    """ Conjugate Gradient Method. """
+    """ MYCG(A,B,maxiter,tol,x0) solves the system of linear equations A*X=B """
+    """ for X. The N-by-N coefficient matrix A must be symmetric and the right"""
+    """hand side column vector B must have length N."""
+    
+    r = A*x-b;
+    d = -r;
+    #rsold = r'*r;
+
+    for i=1:maxiter:
+        Ad = A*d;
+        #alpha = rsold/(d'*Ad);
+        x = x+alpha*d;
+        r = r+alpha*Ad;
+        #rsnew = r'*r;
+        if sqrt(rsnew)<tol:
+            break
+
+        beta = rsnew/rsold;
+        d = -r+beta*d;
+        rsold = rsnew;
+    end
+
+
+
 # parameters
 sigma = 1.0
 mu    = 10.0
