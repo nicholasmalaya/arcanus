8d120858e42a6f1f7988fc3727981ff8206f900f
Nicholas Malaya <nick@ices.utexas.edu>
2014-03-03 15:18:44 -0600

    updating and ready to print

--- inv_prob/ps2/1_3dpl.py
+++ inv_prob/ps2/1_3dpl.py
@@ -2,16 +2,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-# This example demonstrates mplot3d's offset text display.
-# As one rotates the 3D figure, the offsets should remain oriented
-# same way as the axis label, and should also be located "away"
-# from the center of the plot.
-#
-
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 X, Y = np.mgrid[-np.pi/2.:np.pi/2:0.1, -10*np.pi/2:10*np.pi/2.:0.1]
-#Z = np.sqrt(np.abs(np.cos(X) + np.cos(Y)))
 Z = - np.cos(X) * np.cos(Y/10)
 
 surf = ax.plot_surface(X, Y, Z, cmap='autumn', cstride=4, rstride=4)
@@ -19,7 +12,6 @@ ax.set_title("-cos(x)cos(y/10)")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("f(x,y)")
-#ax.set_zlim(0, 2)
 
 plt.savefig("3d.pdf")
 plt.show()
