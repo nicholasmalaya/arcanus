8f3d36f175595074e1c9dcbb23da2c087c8dab6d
Nicholas Malaya <nicholas.malaya@gmail.com>
2014-03-11 12:08:45 -0500

    adding earth gravity computation

---
+++ uq/ps2/earth.py
@@ -0,0 +1,17 @@
+#!/bin/py
+#
+
+G = 6.67384 * 10**-11
+M = 5.97219 * 10**24
+r = 6.371   * 10**6
+rv = r + 35
+print 'G : ', G
+print 'M : ', M
+print 'r : ', r
+print 'rv: ', rv
+print 'g: ', G*M/(rv*rv)
+
+#
+# nick
+# 3/11/14
+#
