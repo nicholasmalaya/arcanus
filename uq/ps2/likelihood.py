4effe11bae8b38e1678511c0a1acbd89cfe80aa2
Nicholas Malaya <nicholas.malaya@gmail.com>
2014-03-26 21:00:51 -0500

    looks like this is either working, or close to working

--- uq/ps2/likelihood.py
+++ uq/ps2/likelihood.py
@@ -2,13 +2,7 @@
 #
 import numpy as np
 from settings_infer import *
-import sys.exit(1)
-
-#
-# precompute whatever we can
-#
-t1 = np.linspace(0.0,2.9,43)
-t2 = np.linspace(0.0,2.9,43)
+import sys
 
 #
 # B  = bowling
@@ -16,11 +10,13 @@ t2 = np.linspace(0.0,2.9,43)
 #
 g = 9.81334 
 rho=1.4
+tau=0.5
+
 rb =0.1166
 rbb=0.1066
+
 Mb =2.296
 Mbb=.4548
-tau=0.5
 
 #
 # load bowling ball data
@@ -43,32 +39,110 @@ tb2  = tb2/600.0
 tbb1 = tbb1/600.0
 tbb1 = tbb1/600.0
 
+nb1  = len(tb1)
+nb2  = len(tb2)
+nbb1 = len(tbb1)
+nbb2 = len(tbb2)
+
+#
+# initalize covariance matricies as zeros
+#
+Rb1  = np.zeros((nb1,  nb1))
+Rb21 = np.zeros((nb2,  nb2))
+Rbb1 = np.zeros((nbb1,  nbb1))
+Rbb2 = np.zeros((nbb2,  nbb2))
+
+#
+# now, iterate over each matrix element
+#
+for i in xrange(len(tb1)):
+    for j in xrange(len(tb1)):
+        Rb1[i][j] = np.exp(-abs(tb1[i]-tb1[j])/tau)
+
+for i in xrange(len(tb2)):
+    for j in xrange(len(tb2)):
+        Rb2[i][j] = np.exp(-abs(tb2[i]-tb2[j])/tau)
+
+for i in xrange(len(tbb1)):
+    for j in xrange(len(tbb1)):
+        Rbb1[i][j] = np.exp(-abs(tbb1[i]-tbb1[j])/tau)
+
+for i in xrange(len(tbb2)):
+    for j in xrange(len(tbb2)):
+        Rbb2[i][j] = np.exp(-abs(tbb2[i]-tbb2[j])/tau)
+
 def likelihood(params):
     """
     This routine should return the log of the
     likelihood function: P(qi|q,C,p,X)
     """
-    a, s, h, c = params
+    a, s, H, c = params
 
     #
-    # form r's
+    # form r's (coefficient on ODE)
     # 
-    rb  = c*rho*4*np.pi*r1**2/(2*Mb)
-
-    rbb = c*rho*4*np.pi*r2**2/(2*Mbb)
+    rb  = (c*rho*4*np.pi*r1**2.0)/(2.0*Mb)
+    rbb = (c*rho*4*np.pi*r2**2.0)/(2.0*Mbb)
     
+    #
+    # sanity check
+    #
     if r < 0 or r2 < 0:
-        print 'achtung! negative drag!'
+        print "achtung! negative drag!"
         sys.exit(1)
 
     #
-    # solve ode at each time
+    # solve ode at each time series
     #
-    [h1,v2]=drag_eqn(t1,g,r)
-    [h2,v2]=drag_eqn(t2,g,r2)
+    [hdb1,vb1]   = drag_eqn(tb1,g,rb)
+    [hdb2,vb2]   = drag_eqn(tb2,g,rb)
 
-    
+    [hdbb1,vbb1] = drag_eqn(tbb1,g,rbb)
+    [hdbb2,vbb2] = drag_eqn(tbb2,g,rbb)
 
-    
-    likelihood = exponent - np.log((2*np.pi)**(n/2) * * (2*np.pi())**(n2/2) * D1 * D2)
-    return likelihood
+    #
+    # form \mu 
+    #
+    mub1  = a * tb1
+    mub2  = a * tb2
+    mubb1 = a * tbb1
+    mubb2 = a * tbb2
+
+    #
+    # form actual covariance
+    #
+
+    sRb1 = s*s * Rb1
+    sRb1 = s*s * Rb1
+    sRb1 = s*s * Rb1
+    sRb1 = s*s * Rb1
+
+    #
+    # calculate determinant
+    #
+    DRb1 = (np.linalg.det(sRb1))**0.5
+    DRb2 = (np.linalg.det(sRb2))**0.5
+    DRbb1= (np.linalg.det(sRbb1))**0.5
+    DRbb2= (np.linalg.det(sRbb2))**0.5
+
+    #
+    # put it all together, and straight on till morning
+    #    
+
+    eb1   = -1/2 * ( 35*hdb1 /H - hd1 - mu) * np.linalg.inv(sRb1) * ( 35*hdb1 /H - hd1 - mub1) 
+    eb2   = -1/2 * ( 35*hdb2 /H - hd2 - mu) * np.linalg.inv(sRb2) * ( 35*hdb2 /H - hd2 - mub2) 
+
+    ebb1   = -1/2 * ( 35*hdbb1 /H - hd1 - mu) * np.linalg.inv(sRbb1) * ( 35*hdbb1 /H - hd1 - mubb1)
+    ebb2   = -1/2 * ( 35*hdbb2 /H - hd2 - mu) * np.linalg.inv(sRbb2) * ( 35*hdbb2 /H - hd2 - mubb2) 
+
+    #
+    # likelihood
+    #
+
+    lb1 = eb1 - np.log((2*np.pi)**(nb1/2) * DRb1)
+    lb2 = eb2 - np.log((2*np.pi)**(nb2/2) * DRb2)
+
+    lbb1 = ebb1 - np.log((2*np.pi)**(nbb1/2) * DRbb1)
+    lbb2 = ebb2 - np.log((2*np.pi)**(nbb2/2) * DRbb2)
+
+    return lb1
