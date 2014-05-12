221ecbef686c09736f9198e1afe200a969dea3e3
Nicholas Malaya <nick@ices.utexas.edu>
2014-05-12 08:33:23 -0500

    successfully plotting the variance, looks intuitively reasonable.

--- combustion/final/flow.py
+++ combustion/final/flow.py
@@ -14,8 +14,10 @@ def zprime(z):
 # main function 
 #
 if __name__ == "__main__":
-
+    """ run a simulation, and plot everything up"""
+    #
     # define field
+    #
     nx = 50
     ny = 91
     
@@ -43,28 +45,46 @@ if __name__ == "__main__":
 
     u = 100 # 100 centimeters = 1 m/s
     
+    # --------------------------------------------------
     #
     # spatial iteration loop!
     #
+    # --------------------------------------------------
     zf = []
     zf.append(z)
+
+    zzf = []
+    zzf.append(np.zeros(ny))
+
     for i in xrange(nx):
 
         zt = np.zeros(ny)
         zt[0] = 1.0
         for j in xrange(1,ny-1):
             zt[j] = DT*dx*(z[j+1]-2*z[j]+z[j-1])/(u*dy*dy) + z[j]
-
+            
         #
         # update mean field  and save state
         #
         z = zt
         zf.append(z)    
-        #print (i+1)*dx, 'cm ',z
 
+        #
+        # calculate fluctuation \bar (z'z')
+        #
+        zzt = np.zeros(ny)
+        for j in xrange(1,ny-1):
+            #zzt[j] = 0.5*((z[j+1]-z[j])/dy)**2 * DT * (i*dx)/u
+            zzt[j] = 0.25*((z[j+1]-z[j-1])/dy)**2 * DT * (i*dx)/u
+        
+        zzf.append(zzt)
+
+    # --------------------------------------------------   
     #
     # plot solution of mean field 
     #
+    # --------------------------------------------------
+        
     y = np.arange(-dy*ny/2., dy*ny/2., dy)
     ind=0
     t='x = '+str(ind*dx)+' cm'
@@ -91,12 +111,48 @@ if __name__ == "__main__":
 
 
     #
-    # plot
+    # plot mean field
     #
     pylab.xlim([-5,5])
     pylab.ylim([-0.1,1.1])
     pylab.legend()
+    pylab.savefig('mean.pdf')
+    pylab.close()
+
+    # --------------------------------------------------
+    #
+    # calculate and plot fluctuation!
+    #
+    # --------------------------------------------------
+
+    ind=0
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zzf[ind], linewidth=2.0, label=t)
+    pylab.xlabel('y (cm)',size=22.0)
+    pylab.ylabel(r'$\bar{z^{\prime}z^{\prime}}$', size=30.0)
+    pylab.title(r'Fluctuating value of the mixture fraction')
+    pylab.grid(True)
+
+    ind=4
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zzf[ind], linewidth=2.0, label=t)
+
+    ind=30
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zzf[ind], linewidth=2.0, label=t)
+
+    ind=50
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zzf[ind], linewidth=2.0, label=t)
+
+    #
+    # plot fluctuation
+    #
+    pylab.legend()
+    pylab.savefig('fluc.pdf')
     pylab.show()
+    
+    
 
 #
 # nick 
