c33e4e55cd606e3b26664876bc0e6adabcb5af56
Nicholas Malaya <nick@ices.utexas.edu>
2014-05-12 07:51:49 -0500

    updating with some mean mixture fraction plotting, looks roughly ok

--- combustion/final/flow.py
+++ combustion/final/flow.py
@@ -4,6 +4,7 @@
 #
 import sys
 import numpy as np
+import pylab
 
 def zprime(z):
     """Returns \bar z'z'"""
@@ -15,8 +16,8 @@ def zprime(z):
 if __name__ == "__main__":
 
     # define field
-    nx = 5
-    ny = 11
+    nx = 50
+    ny = 31
     
     # sanity check
     if(ny%2 != 1):
@@ -33,8 +34,8 @@ if __name__ == "__main__":
     z[0:ny/2] = 1.0
 
     # spacing
-    dx = 5 # centimeters
-    dy = 5 # centimeters
+    dx = 1 # centimeters
+    dy = 1 # centimeters
 
     # Diffusivities
     DL = 0.176 # (cm^2/sec)
@@ -45,18 +46,54 @@ if __name__ == "__main__":
     #
     # spatial iteration loop!
     #
-    print z
+    zf = []
+    zf.append(z)
     for i in xrange(nx):
 
         zt = np.zeros(ny)
         zt[0] = 1.0
         for j in xrange(1,ny-1):
             zt[j] = DT*dx*(z[j+1]-2*z[j]+z[j-1])/(u*dy*dy) + z[j]
-        # update mean field 
-        z = zt
-        
-        print z
 
+        #
+        # update mean field  and save state
+        #
+        z = zt
+        zf.append(z)    
+        #print (i+1)*dx, 'cm ',z
+
+    #
+    # plot solution of mean field 
+    #
+    y = np.arange(-dy*ny/2., dy*ny/2., dy)
+    ind=0
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zf[ind], linewidth=2.0, label=t)
+    pylab.xlabel('y (cm)',size=22.0)
+    pylab.ylabel(r'$\bar z$', size=30.0)
+    pylab.title(r'Mean value of the mixture fraction, $\bar z$')
+    pylab.grid(True)
+
+    #
+    # 5 cm, 30 cm and 50 cm
+    #
+    ind=5
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zf[ind], linewidth=2.0, label=t)
+
+    ind=30
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zf[ind], linewidth=2.0, label=t)
+
+    ind=50
+    t='x = '+str(ind*dx)+' cm'
+    pylab.plot(y,zf[ind], linewidth=2.0, label=t)
+
+
+    #
+    # plot
+    #
+    pylab.show()
 
 #
 # nick 
