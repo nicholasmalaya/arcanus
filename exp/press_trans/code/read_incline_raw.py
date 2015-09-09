e5e41743d3fff5a2b3c115996c373c2f026fdcc9
Nicholas Malaya <nick@ices.utexas.edu>
2015-09-09 14:56:20 -0500

    working. now with sqr fit

--- exp/press_trans/code/read_incline_raw.py
+++ exp/press_trans/code/read_incline_raw.py
@@ -4,20 +4,47 @@
 # read contents
 # (re)start when third column found
 #
+import sys
+
+
+#
+# open and read file
+#
 path="../data/alldata_incl.lvm"
 file = open(path, "r+")
 
-for line in file:
+#
+# data objects
+#
+set_names=[]
 
+for line in file:
+    #
+    # sep by whitespace
+    #
     line_list = line.split()
 
-    #
-    # reset!
-    #
     if( len(line_list) > 2):
+        #
+        # reset! save name of this set
+        #
         set_name=line_list[2:]
-        print set_name
+        set_names.append(' '.join(set_name))
         
+        
+
+    else:
+        b=1
+
+#
+# clean up
+#
+file.close()       
+
+#
+# steady as she goes
+#
+sys.exit(0)
 
 #
 # nick 
--- exp/press_trans/code/read_incline_stats.py
+++ exp/press_trans/code/read_incline_stats.py
@@ -42,12 +42,21 @@ for line in file:
         height.append(line_list[8])
         voltage.append(line_list[2])
         std.append(line_list[3])
-        print line_list
 #
 # clean up
 #
 file.close()       
 
+#
+# least squares curve fit
+#
+import numpy as np
+from scipy import stats
+height  = [float(i) for i in height]
+voltage = [float(i) for i in voltage]
+(slope, intercept, r_value, p_value, std_err) = stats.linregress(height,voltage)
+print "r-squared:", r_value**2
+print  'p_value', p_value
 #
 # plot it!
 #
@@ -55,6 +64,10 @@ import matplotlib.pyplot as plt
 plt.subplot(2, 1, 1)
 plt.plot(height, voltage,  'ko',label='First Calibration Set',color='blue')
 plt.plot(height2, voltage2,'ko',label='Second Calibration Set',color='black')
+
+line = slope*np.array(height) + intercept
+print line
+plt.plot(height, line, '--k',label='Least Squares Fit') 
 plt.title('Calibration of an Pressure Transducer: Inclined Manometer')
 plt.ylabel('Voltage')
 plt.xlabel('Height (Inches)')
