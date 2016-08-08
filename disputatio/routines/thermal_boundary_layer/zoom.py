91afe430d3b84a897dbbcf7ad16341ddf5834f33
Nicholas Malaya <nick@ices.utexas.edu>
2016-08-08 15:34:28 -0500

    working on fit

--- disputatio/routines/thermal_boundary_layer/zoom.py
+++ disputatio/routines/thermal_boundary_layer/zoom.py
@@ -25,30 +25,51 @@ def read_dat():
     
     return x,t
 
+def e(x):
+    c = 150.0
+    exxp = 42+20*np.exp(-np.array(x)*c)
+    return exxp
+
 #
 #
 #
 def plot_raw(x,t):
     mx = 5
-    order = 7
+    order = 4
     print x[0:mx],t[0:mx]
     pp = np.poly1d(np.polyfit(x[0:mx],t[0:mx],order))
-    xx = np.arange(0,x[mx],.005)
+    xx = np.arange(0,x[mx],.01)
 
     #log_lst = np.log(lst)
     #print log_lst
     plt.subplot(1, 1, 1)
     plt.plot(x,t, 'ko-',color='blue',label='Raw')
-    plt.plot(xx,pp(xx), 'ko-',color='red',label='fit')
     plt.title('Thermal Boundary Layer Measured')
     plt.xlabel('Location Above Ground (Meters)')
     plt.ylabel('Temperature (Celsius)')
     plt.legend()
 
-    plt.xlim((0,.35))
-    plt.ylim((40,65))
+    full=1
 
+    if(full):
+        plt.xlim((0,.35))
+        plt.ylim((40,65))
+    else:
+        plt.xlim((0,2.35))
+        plt.ylim((35,65))
+        
+    # plot exponential
+    plt.plot(xx,e(xx), 'ko-',color='red',label='exponential')
+
+    # plot polynomial:
+    #plt.plot(xx,pp(xx), 'ko-',color='red',label='fit')
+
+    #
+    ## execute!
+    #
     plt.show()
+
+    print 'Peak velocity modelled is: ',e(0)
 
 def main():
     # my code here
