b06f759090901eb4badfe59198a73a86e884ff6a
Nicholas Malaya <nick@ices.utexas.edu>
2016-08-08 15:52:52 -0500

    well, thats that

--- disputatio/routines/thermal_boundary_layer/zoom.py
+++ disputatio/routines/thermal_boundary_layer/zoom.py
@@ -19,47 +19,56 @@ def read_dat():
         values = line.split("\t")
         values = line.split("\t")
         x.append(float(values[0]))
-        print values[1].split("\n")[0]
+        #print values[1].split("\n")[0]
         t.append(float(values[1].split("\n")[0]))
     f.close()
     
     return x,t
 
-def e(x):
-    c = 150.0
-    exxp = 42+20*np.exp(-np.array(x)*c)
-    return exxp
+def e(x, a, b, c):
+    return a * np.exp(-b * x) + c
+
+#def e(x):
+#    c = 240.0
+#    exxp = 42+30*np.exp(-np.array(x)*c)
+#    return exxp
 
 #
 #
 #
 def plot_raw(x,t):
-    mx = 5
+    mx = 4
     order = 4
-    print x[0:mx],t[0:mx]
-    pp = np.poly1d(np.polyfit(x[0:mx],t[0:mx],order))
+    print x[0:mx]
+    print t[0:mx]
+    #pp = np.poly1d(np.polyfit(x[0:mx],t[0:mx],order))
     xx = np.arange(0,x[mx],.001)
+
+    #
+    # find ideal exp coefficients
+    #
+    from scipy.optimize import curve_fit
+    popt, pcov = curve_fit(e, x[0:mx], t[0:mx])
+
+    print popt
 
     #log_lst = np.log(lst)
     #print log_lst
     plt.subplot(1, 1, 1)
-    plt.plot(x,t, 'ko-',color='blue',label='Raw')
+    plt.plot(x,t, 'ko',color='blue',label='Raw',markersize=10)
     plt.title('Thermal Boundary Layer Measured')
     plt.xlabel('Location Above Ground (Meters)')
     plt.ylabel('Temperature (Celsius)')
     plt.legend()
 
-    full=1
-
-    if(full):
-        plt.xlim((0,.35))
-        plt.ylim((40,65))
-    else:
-        plt.xlim((0,2.35))
-        plt.ylim((35,65))
+    plt.xlim((0,.35))
+    plt.ylim((40,75))
         
+    #
+    # http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+    #
     # plot exponential
-    plt.plot(xx,e(xx), 'ko-',color='red',label='exponential')
+    plt.plot(xx,e(xx,popt[0],popt[1],popt[2]), 'ko-', color='red',label='exponential')
 
     # plot polynomial:
     #plt.plot(xx,pp(xx), 'ko-',color='red',label='fit')
@@ -69,7 +78,13 @@ def plot_raw(x,t):
     #
     #plt.show()
     plt.savefig('fit_zoomed.png')
-    print 'Peak velocity modelled is: ',e(0)
+
+    # unzoom
+    plt.xlim((0,2.35))
+    plt.savefig('fit.png')
+
+    print 'Peak velocity modelled is: ',e(0,popt[0],popt[1],popt[2])
+    print 'Gradient is: ', e(0,popt[0],popt[1],popt[2])-t[-1]
 
 def main():
     # my code here
