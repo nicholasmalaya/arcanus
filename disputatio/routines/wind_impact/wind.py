f44bfce4fdd3cabf819ad741a0bdad9b6457adad
Nicholas Malaya <nick@ices.utexas.edu>
2016-10-14 14:50:53 -0500

    updating with impact of wind

--- disputatio/routines/wind_impact/wind.py
+++ disputatio/routines/wind_impact/wind.py
@@ -7,6 +7,37 @@
 # 0        20.0  | 0.0
 #
 
+speed = [0,1,2,3]
+hot   = [20.0,34.5,40.09,34.9]
+cold  = [0.0,.8745,9.3,29.8]
+
+
+#
+#
+#
+import matplotlib.pyplot as plt
+
+plt.plot(speed,hot,'o', label='Hot')
+plt.plot(speed,cold,'o', label='Cold')
+
+#overrx = np.linspace(1.0, 5.0, num=50)
+plt.xlim([0,5])
+plt.ylim([0,45])
+
+fsz=20
+plt.ylabel(r'Kinetic Energy Flux (Watts)',
+           fontsize=fsz,labelpad=20)
+
+plt.xlabel(r'Wind Speed (m/s)',fontsize=fsz)
+
+
+# might not need this
+#plt.axes().set_aspect('equal', 'datalim')
+
+plt.legend(loc='best')
+plt.tight_layout()
+#plt.show()
+plt.savefig('wind_impact.png')
 
 
 #
